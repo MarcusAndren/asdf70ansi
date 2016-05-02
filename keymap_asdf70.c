1c4d63742737aba9684e4d2f8d240cd6c5dd4166
MarcusAndren <andren.marcus@gmail.com>
2016-05-02 21:42:54 +0200

    updated layers

--- keymap_asdf70.c
+++ keymap_asdf70.c
@@ -3,28 +3,28 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 0: qwerty ansi */
   KEYMAP(
-        ESC, F1, F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11,   F12, FN1, DEL, INS,  \
-        GRV, 1,   2,        3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,PGUP, \
-        TAB,      Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,PGDN, \
+        ESC, F1, F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9, F10, F11,   F12, INS, HOME,PGUP,  \
+        GRV, 1,   2,        3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,PGDN, \
+        TAB,      Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,DEL, \
         CAPS,     A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT, END,  \
-        LSFT,          Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,UP,  HOME, \
-        LCTL,     LGUI,LALT,        SPC,                  RALT,RCTL,FN0, LEFT,DOWN,RIGHT),
+        LSFT,          Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,UP,  FN1, \
+        LCTL,     LGUI,LALT,        SPC,                  RALT,FN0, RCTL,LEFT,DOWN,RIGHT),
 
   /* 1: FN 1 ansi */
   KEYMAP(
-        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,TRNS, \
-        TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,MUTE, \
-        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,MNXT, \
-        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       ENT, MPRV, \
-        TRNS,             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, UP,  MPLY, \
+        TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MPRV, MPLY, MNXT, MUTE, VOLD, VOLU, TRNS, TRNS,TRNS, \
+        TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,TRNS, \
+        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,TRNS, \
+        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       ENT, TRNS, \
+        TRNS,             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, UP,  TRNS, \
         TRNS,       TRNS, TRNS,			 			  TRNS, 									TRNS, TRNS, TRNS, LEFT, DOWN,RIGHT),
 
   /* 1: FN 2 ansi */
   KEYMAP(
         TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
-        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, VOLU, \
-        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, VOLD, \
+        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+        TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, \
         TRNS,             TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
         TRNS,       TRNS, TRNS, 						TRNS,									  TRNS, TRNS, TRNS, TRNS, TRNS, TRNS),
 };
